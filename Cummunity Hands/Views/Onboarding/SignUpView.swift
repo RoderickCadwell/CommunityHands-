e0f1e03ae56db87664d0417dde3f753a35ea56b6
Tone King <tone@tonekingdev.com>
2026-07-27 14:49:53 -0400

    update: added WelcomeView for entry point

--- Cummunity Hands/Views/Onboarding/SignUpView.swift
+++ Cummunity Hands/Views/Onboarding/SignUpView.swift
@@ -1,5 +1,5 @@
 //
-//  WelcomeView.swift
+//  SignUpView.swift
 //  Cummunity Hands
 //
 //  Created by JOURNi Student on 7/27/26.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct WelcomeView: View {
+struct SignUpView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    WelcomeView()
+    SignUpView()
 }
